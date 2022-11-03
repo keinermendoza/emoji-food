921508334b82cf5c82490aca34e854cc9ba5dc5d
Keiner Mendoza <keinermendoza@gmail.com>
2022-11-03 14:14:27 -0300

    pasando funciones de los archivos html al actions.js

--- static/action.js
+++ static/action.js
@@ -24,5 +24,54 @@ window.onload = function()
         }
     });
     
+    // Using emojis for show and hide the password
+
+    document.getElementById('password-emoji').addEventListener('click', function() {
+        
+        if (this.innerHTML == '🙈') {
+            this.innerHTML = '👀';
+            document.getElementById('password').type = "text";    
+        } 
+        else
+        {
+            this.innerHTML = '🙈';
+            document.getElementById('password').type = "password";
+        }
+    });
+    document.getElementById('confirmation-emoji').addEventListener('click', function() {
+        if (this.innerHTML == '🙈') {
+            this.innerHTML = '👀';
+            document.getElementById('confirmation').type = "text";    
+        } 
+        else
+        {
+            this.innerHTML = '🙈';
+            document.getElementById('confirmation').type = "password";
+        }
+    });
+
+    // checking if password an confirmation-password match on-live
+    // I toke this function from https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
+    var match = document.getElementById('password-state-match');
+
+    [document.getElementById('password'), document.getElementById("confirmation")].forEach(item => {
+        item.addEventListener('input', event => {
+            password = document.getElementById('password').value;
+            confirmation = document.getElementById("confirmation").value;
+
+            if (password == "" || confirmation == "") {
+                match.innerHTML = "";
+            }
+            else if (password == confirmation) {
+                match.innerHTML = "Password match &#128077";
+            }
+            else {
+                match.innerHTML = "Password not match yet &#129300";
+            }
+        })
+    })
+    
+
+
 }
 
